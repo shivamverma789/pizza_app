1f94eb127c8b61c0f497ca1cc18ef9ec47581114
shivamverma789 <shivamjitendraverma@gmail.com>
2024-04-13 15:13:14 +0530

    added login and register page

--- server.js
+++ server.js
@@ -6,15 +6,30 @@ const path =require("path");
 
 const PORT = process.env.PORT || 3000;
 
+app.use(expressLayout);
+app.set("views",path.join(__dirname, "/resources/views"))
+app.set ("view engine", "ejs");
+
+app.use(express.static("public"));
+
 app.get("/",(req,res)=>{
     res.render("home");
 })
 
-app.use(express.static("public"));
+app.get("/cart",(req,res)=>{
+    res.render("customers/cart");
+})
 
-app.use(expressLayout);
-app.set("views",path.join(__dirname, "/resources/views"))
-app.set ("view engine", "ejs");
+app.get("/login",(req,res)=>{
+    res.render("auth/login");
+})
+
+app.get("/register",(req,res)=>{
+    res.render("auth/register");
+})
+
+
+
 
 app.listen(3000, ()=>{
     console.log(`listening on the port ${PORT}`)
